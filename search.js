13bcbe5b6d11c2ecff50a24a6f41363d6ef436bb
Bilal140202 <bilalansari1402@gmail.com>
2025-02-23 03:19:04 +0530

    Update search.js

--- search.js
+++ search.js
@@ -2,14 +2,14 @@ const performSearch = (query, sites = [], filetypes = []) => {
   let searchParams = [encodeURIComponent(query)];
 
   if (sites.length) {
-    searchParams.push(`(${sites.map(site => `site:${site}`).join(' OR ')})`);
+    searchParams.push(sites.map(site => `site:${encodeURIComponent(site)}`).join(' OR '));
   }
 
   if (filetypes.length) {
-    searchParams.push(`(${filetypes.map(filetype => `filetype:${filetype}`).join(' OR ')})`);
+    searchParams.push(filetypes.map(filetype => `filetype:${encodeURIComponent(filetype)}`).join(' OR '));
   }
 
-  const searchQuery = `https://www.google.com/search?q=${searchParams.join('+')}`;
+  const searchQuery = `https://www.google.com/search?q=${searchParams.join(' ')}`;
   window.open(searchQuery, '_blank');
 };
 
