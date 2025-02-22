dfe4fdf604840590a1bfd177a53122f382712aed
Bilal140202 <bilalansari1402@gmail.com>
2025-02-23 03:51:20 +0530

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,20 @@
+const config = {
+  defaultSites: [
+    "wikipedia.org",
+    "stackoverflow.com",
+    "github.com",
+    "medium.com",
+    "reddit.com"
+  ],
+  supportedFileTypes: [
+    "pdf",
+    "docx",
+    "xlsx",
+    "pptx",
+    "txt",
+    "zip"
+  ],
+  theme: "light", // Default theme (light/dark)
+};
+
+export default config;
